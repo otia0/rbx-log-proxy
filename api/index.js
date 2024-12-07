c9e5b83ff34ea59687a613b0490b594194f92ae6
otia0 <aitoforms@gmail.com>
2024-12-07 15:47:34 +0200

    Update #2

--- api/index.js
+++ api/index.js
@@ -6,13 +6,16 @@ module.exports = async (req, res) => {
     if (req.method === 'POST') {
         try {
             const payload = req.body;
+            console.log('Received data:', payload);  // Для отладки
+
             await axios.post(discordWebhook, payload);
             res.status(200).send('Message sent to Discord');
         } catch (error) {
-            console.error(error.message);
+            console.error('Error sending message:', error.message);
             res.status(500).send('Error sending message to Discord');
         }
     } else {
+        // Если метод не POST, возвращаем ошибку 405 (Method Not Allowed)
         res.status(405).send('Method Not Allowed');
     }
 };
